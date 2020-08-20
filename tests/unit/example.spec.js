4e63bc5ba0c93601c4a9e7f6b6bb6519e331f2b0
Artur <Artur.Hoppner@hotmail.com>
2020-08-20 20:55:02 +0200

    test to pass

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -7,6 +7,6 @@ describe("HelloWorld.vue", () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(ms);
+    expect(wrapper.text()).toMatch(msg);
   });
 });
