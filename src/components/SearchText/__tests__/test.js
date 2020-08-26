710d2dae4e51cbb8d783bcf42f02366c39afe982
Artur <Artur.Hoppner@hotmail.com>
2020-08-26 21:05:11 +0200

    added missing files

---
+++ src/components/SearchText/__tests__/test.js
@@ -0,0 +1,7 @@
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import store from '@/store/index';
+import searchtext from '@/components/SearchText/SearchText.vue';
+
+it('');
+// wrapper.array.length
